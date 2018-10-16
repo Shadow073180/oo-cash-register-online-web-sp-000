5ac40d5be5464358fb6da442d58c34175abbaaf3
David masterson <david73180@gmail.com>
2018-10-16 03:37:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
 
 
   def add_item(item, price, quantity = 1)
-    @total = quantity * price
+    @total += (quantity * price)
       quantity.times do
         @item << item
       end
