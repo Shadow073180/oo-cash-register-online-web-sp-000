ef810d6edee69b4aa33624db2c838c7415155b56
David masterson <david73180@gmail.com>
2018-10-16 03:24:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
 
 
-  def add_item(title, price, quantinty = nil)
+  def add_item(title, price, quantinty = 1)
     register = CashRegister.new
     @total = @total + price
     @quantity = quantity
